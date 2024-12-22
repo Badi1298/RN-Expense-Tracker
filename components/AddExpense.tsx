36a4ab32fef39a31d4f9455d602e692116ccc6c9
Badi1298 <serbandavid83@yahoo.com>
2024-12-22 22:53:25 +0200

    Add AddExpense component with input fields and BaseButton for expense entry

--- components/AddExpense.tsx
+++ components/AddExpense.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, Modal, Pressable, StyleSheet } from 'react-native';
+import { View, Text, Modal, StyleSheet, TextInput } from 'react-native';
+
+import BaseButton from './ui/BaseButton';
 
 type Props = {
     showModal: boolean;
@@ -7,21 +9,36 @@ type Props = {
 };
 
 export default function AddExpense({ showModal, onHideModal }: Props) {
-    function pressHandler() {
+    function hideModalHandler() {
         onHideModal();
+    }
+
+    function addExpenseHandler() {
+        // Add expense logic here
     }
 
     return (
         <Modal animationType="fade" transparent={true} visible={showModal}>
             <View style={styles.centeredView}>
                 <View style={styles.modalView}>
-                    <Text style={styles.modalText}>Hello World!</Text>
-                    <Pressable
-                        style={[styles.button, styles.buttonClose]}
-                        onPress={pressHandler}
-                    >
-                        <Text style={styles.textStyle}>Close</Text>
-                    </Pressable>
+                    <Text style={styles.modalText}>Add Expense</Text>
+                    <View style={styles.inputContainer}>
+                        <Text>Expense Title:</Text>
+                        <TextInput style={styles.textInput} />
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <Text>Expense Amount:</Text>
+                        <TextInput style={styles.textInput} />
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <Text>Expense Date:</Text>
+                        <TextInput style={styles.textInput} />
+                    </View>
+
+                    <View style={styles.buttonsContainer}>
+                        <BaseButton title="Close" onPress={hideModalHandler} />
+                        <BaseButton title="Save" onPress={addExpenseHandler} />
+                    </View>
                 </View>
             </View>
         </Modal>
@@ -32,7 +49,6 @@ const styles = StyleSheet.create({
     centeredView: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
     },
     modalView: {
         margin: 20,
@@ -49,24 +65,26 @@ const styles = StyleSheet.create({
         shadowRadius: 4,
         elevation: 5,
     },
-    button: {
-        borderRadius: 20,
-        padding: 10,
-        elevation: 2,
-    },
-    buttonOpen: {
-        backgroundColor: '#F194FF',
-    },
-    buttonClose: {
-        backgroundColor: '#2196F3',
-    },
-    textStyle: {
-        color: 'white',
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
+
     modalText: {
+        fontSize: 24,
+        fontWeight: 'bold',
         marginBottom: 15,
         textAlign: 'center',
+    },
+
+    inputContainer: {
+        width: '100%',
+        rowGap: 4,
+    },
+    textInput: {
+        borderColor: 'black',
+        borderWidth: 1,
+        marginBottom: 15,
+    },
+
+    buttonsContainer: {
+        flexDirection: 'row',
+        columnGap: 16,
     },
 });
---
+++ components/ui/BaseButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text, Modal, Pressable, StyleSheet } from 'react-native';
+
+type Props = {
+    title: string;
+    onPress: () => void;
+};
+
+export default function BaseButton({ title, onPress }: Props) {
+    return (
+        <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={onPress}
+        >
+            <Text style={styles.textStyle}>{title}</Text>
+        </Pressable>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonClose: {
+        backgroundColor: '#2196F3',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+});
