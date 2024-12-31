a22da3ef7aee2cb30b557dd7562512feeac9f491
Badi1298 <serbandavid83@yahoo.com>
2024-12-31 16:47:06 +0200

    Add logout functionality to RootBottomTabs with confirmation alert

--- navigation/RootBottomTabs.tsx
+++ navigation/RootBottomTabs.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import { useDispatch } from 'react-redux';
+import { logout } from '../store/slices/authSlice';
+
 import { Ionicons } from '@expo/vector-icons';
 
 import AllExpensesScreen from '../screens/AllExpensesScreen';
 import RecentExpensesScreen from '../screens/RecentExpensesScreen';
+import { Alert } from 'react-native';
 
 export type RootTabParamsList = {
     RecentExpenses: undefined;
@@ -15,21 +19,48 @@ export type RootTabParamsList = {
 const RootTab = createBottomTabNavigator<RootTabParamsList>();
 
 export default function RootBottomTabs() {
+    const dispatch = useDispatch();
+
     return (
         <RootTab.Navigator
             screenOptions={({ navigation }) => ({
                 headerRight: ({ tintColor }) => (
-                    <Ionicons
-                        name="add"
-                        size={24}
-                        color={tintColor}
-                        style={{ marginRight: 10 }}
-                        onPress={() =>
-                            navigation.navigate('AddExpense', {
-                                expenseId: null,
-                            })
-                        }
-                    />
+                    <>
+                        <Ionicons
+                            name="add"
+                            size={24}
+                            color={tintColor}
+                            style={{ marginRight: 16 }}
+                            onPress={() =>
+                                navigation.navigate('AddExpense', {
+                                    expenseId: null,
+                                })
+                            }
+                        />
+                        <Ionicons
+                            name="log-out-outline"
+                            size={24}
+                            color={tintColor}
+                            style={{ marginRight: 10 }}
+                            onPress={() => {
+                                Alert.alert(
+                                    'Confirm Logout',
+                                    'Are you sure you want to logout?',
+                                    [
+                                        {
+                                            text: 'Cancel',
+                                            style: 'cancel',
+                                        },
+                                        {
+                                            text: 'Logout',
+                                            onPress: () => dispatch(logout()),
+                                        },
+                                    ],
+                                    { cancelable: true }
+                                );
+                            }}
+                        />
+                    </>
                 ),
             })}
         >
